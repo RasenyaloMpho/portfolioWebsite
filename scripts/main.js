b39a73bfb846a2e5b955fba1b98e1766bf7cd288
MphoRasenyalo201576673 <201576673@student.uj.ac.za>
2022-02-14 07:23:56 +0200

    added menu collapse on smaller devices

--- scripts/main.js
+++ scripts/main.js
@@ -16,3 +16,10 @@ window.onload = function() {
 
         });
 }
+// collapse navbar after click on small devices
+const navLinks = document.querySelectorAll('.nav-li')
+const menuToggle = document.getElementById('navbarSupportedContent')
+
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
+})
